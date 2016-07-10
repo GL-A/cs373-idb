ecf9c37a280ed7d40a08e81dbb5c03f8ce14cc7e
Kevin Du <kevind7@cs.utexas.edu>
2016-07-10 18:07:10 -0500

    Updated models.py, added dcdatabase.py

--- app/models.py
+++ app/models.py
@@ -9,26 +9,40 @@ db = SQLAlchemy(app)
 class Character(db.Model):
 
     """
-    DC characters with attributes name, hero/villain alias, an image,
+    DC characters with attributes title, hero/villain alias, an image,
     their alignment, and other information
     """
 
     __tablename__ = 'characters'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    alias = db.Column(db.String(50))
-    image = db.Column(db.String(250))
+    id = db.Column(db.Integer)
+    title = db.Column(db.String(50), primary_key=True)
+    creators = db.Column(ARRAY(db.String(100)))
     alignment = db.Column(db.String(50))
+    identity = db.Column(db.String(50))
+    real_name = db.Column(db.String(50))
+    universe = db.Column(ARRAY(db.String(100)))
+    image = db.Column(db.String(250))
+    status = db.Column(db.String(50))
+    gender = db.Column(db.String(50))
+    debut = db.Column(db.String(50))
+    aliases = db.Column(ARRAY(db.String(100)))
 
-    def __init__(self, name, alias, image, alignment):
+    def __init__(self, title, alias, image, alignment, creators, identity, real_name, universe, status, gender, debut, aliases):
         """
         Initialize the character
         """
-        self.name = name
-        self.alias = alias
+        self.title = title
+        self.aliases = aliases
         self.image = image
         self.alignment = alignment
+        self.creators = creators
+        self.identity = identity
+        self.real_name = real_name
+        self.universe = universe
+        self.status = status
+        self.gender = gender
+        self.debut = debut
 
     def __repr__(self):
         """
@@ -39,168 +53,284 @@ class Character(db.Model):
         """
         return '<alias {}>'.format(self.alias)
 
+    def to_json(self, list_view = False):
+        """
+        Return a dictionary of information of this character
+        """
+        return {
+            'id' : self.id,
+            'title' : self.title,
+            'creators' : self.creators,
+            'alignment' : self.alignment,
+            'identity' : self.identity,
+            'real_name' : self.real_name,
+            'universe' : self.universe,  
+            'image' : self.image,
+            'status' : self.status,
+            'gender' : self.gender,
+            'debut' : self.debut,
+            'aliases' : self.aliases
+            }
+
 
 class Teams(db.Model):
 
     """
-    DC teams with attributes name, first comic appeared in,
+    DC teams with attributes title, first comic appeared in,
     and other information
     """
 
     __tablename__ = 'teams'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
+    id = db.Column(db.Integer)
+    title = db.Column(db.String(100), primary_key=True)
     image = db.Column(db.String(250))
-    first_appearance = db.Column(db.String(100))
-    base_of_operations = db.Column(db.String(100))
+    debut = db.Column(db.String(100))
+    origin = db.Column(db.String(100))
+    identity = db.Column(db.String(100))
+    status = db.Column(db.String(25))
+    creators = db.Column(ARRAY(db.String(100)))
+    universe = db.Column(ARRAY(db.String(100)))
+    team_leaders = db.Column(ARRAY(db.String(100)))
+    enemies = db.Column(ARRAY(db.String(100)))
 
-    def __init__(self, name, image, first_appearance, base_of_operations):
+    def __init__(self, title, image, debut, origin, identity, status, creators, universe, team_leaders, enemies):
         """
         Initialize a team
         """
-        self.name = name
+        self.title = title
         self.image = image
-        self.first_appearance = first_appearance
-        self.base_of_operations = base_of_operations
+        self.debut = debut
+        self.origin = origin
+        self.identity = identity
+        self.status = status
+        self.creators = creators
+        self.universe = universe
+        self.team_leaders = team_leaders
+        self.enemies = enemies
 
     def __repr__(self):
         """
         Return represenation of this team in format
-        <name {}> where {} is the team's name
+        <title {}> where {} is the team's title
         """
-        return '<name {}>'.format(self.name)
+        return '<title {}>'.format(self.title)
 
+    def to_json(self, list_view = False):
+        """
+        Return a dictionary of information of this teams
+        """
+        return {
+            'id' : self.id,
+            'title' : self.title,
+            'debut' : self.debut,
+            'origin' : self.origin,
+            'identity' : self.identity,
+            'status' : self.status,
+            'creators': self.creators,
+            'universe': self.universe,
+            'team_leaders' : self.team_leaders,
+            'enemies' : self.enemies
+        }
 
 class Comics(db.Model):
 
     """
-    DC comics with attributes name, issue number, writer, date
+    DC comics with attributes title, issue number, writer, date
     published, and other information
     """
 
     __tablename__ = 'comics'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer)
     image = db.Column(db.String(250))
-    name = db.Column(db.String(100))
-    total_issues = db.Column(db.String(5))
+    title = db.Column(db.String(100), primary_key=True)
     date = db.Column(db.String(50))
+    locations = db.Column(ARRAY(db.String(100)))
+    featured_characters = db.Column(ARRAY(db.String(100)))
+    creators = db.Column(ARRAY(db.String(100)))
 
-    def __init__(self, image, name, total_issue, date):
+    def __init__(self, image, title, date, locations, featured_characters, creators):
         """
         Initialize a comic
         """
         self.image = image
-        self.name = name
-        self.total_issue = total_issues
+        self.title = title
         self.date = date
+        self.locations = locations
+        self.featured_characters = featured_characters
+        self.creators = creators
 
     def __repr__(self):
         """
         Return represenation of this comic in format
-        <name {}> where {} is the comic's name
+        <title {}> where {} is the comic's title
         """
-        return '<name {}>'.format(self.name)
+        return '<title {}>'.format(self.title)
+
+    def to_json(self, list_view = False):
+        """
+        Return a dictionary of information of this comic
+        """
+        return {
+            'id' : self.id,
+            'title' : self.title,
+            'image' : self.image,
+            'date' : self.date,
+            'locations' : self.locations,
+            'featured_characters' : self.featured_characters,
+            'creators' : self.creators
+        }
 
 
 class Movies(db.Model):
 
     """
-    DC movies with attributes name, director, date released,
+    DC movies with attributes title, director, date released,
     and other information
     """
 
     __tablename__ = 'movies'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer)
     image = db.Column(db.String(250))
-    name = db.Column(db.String(100))
-    director = db.Column(db.String(50))
+    title = db.Column(db.String(100), primary_key=True)
     date = db.Column(db.String(50))
-    distributors = db.Column(db.String(30))
-    composers = db.Column(db.String(50))
+    running_time = db.Column(db.String(50))
+    budget = db.Column(db.String(50))
+    creators = db.Column(ARRAY(db.String(50)))
+    featured_characters = db.Column(ARRAY(db.String(100)))
 
-    def __init__(self, image, name, director, date, distributors, composers):
+    def __init__(self, image, title, date, running_time, budget, creators, featured_characters):
         """
         Initialize a movie
         """
         self.image = image
-        self.name = name
-        self.director = director
+        self.title = title
         self.date = date
-        self.distributors = distributors
-        self.composers = composers
+        self.running_time = running_time
+        self.budget = budget
+        self.creators = creators
+        self.featured_characters = featured_characters
 
     def __repr__(self):
         """
         Return represenation of this movie in format
-        <name {}> where {} is the movie's name
+        <title {}> where {} is the movie's title
         """
-        return '<name {}>'.format(self.name)
+        return '<title {}>'.format(self.title)
 
+    def to_json(self, list_view = False):
+        """
+        Return a dictionary of information of this movie
+        """
+        return {
+            'id' : self.id,
+            'title' : self.title,
+            'image' : self.image,
+            'date' : self.date,
+            'running_time' : self.running_time,
+            'budget' : self.budget,
+            'featured_characters' : self.featured_characters,
+            'creators' : self.creators
+        }
 
 class Shows(db.Model):
 
     """
-    DC shows with attributes name, network aired on, date first
+    DC shows with attributes title, network aired on, date first
     aired on, and other information
     """
 
     __tablename__ = 'shows'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer)
+    title = db.Column(db.String(100), primary_key=True)
+    last_air  = db.Column(db.String(250))
+    running_time  = db.Column(db.String(250))
     image = db.Column(db.String(250))
-    name = db.Column(db.String(100))
-    network = db.Column(db.String(50))
-    date = db.Column(db.String(50))
+    first_air = db.Column(db.String(50))
+    creators = db.Column(ARRAY(db.String(100)))
+    characters = db.Column(ARRAY(db.String(100)))
 
-    def __init__(self, image, name, network, date):
+    def __init__(self, image, title, network, running_time, first_air, creators, characters, last_air):
         """
         Initialize a show
         """
         self.image = image
-        self.name = name
-        self.network = network
-        self.date = date
+        self.title = title
+        self.last_air  = last_air
+        self.running_time  = running_time
+        self.first_air = first_air
+        self.creators = creators
+        self.characters = characters
 
     def __repr__(self):
         """
         Return represenation of this show in format
-        <name {}> where {} is the show's name
+        <title {}> where {} is the show's title
         """
-        return '<name {}>'.format(self.name)
+        return '<title {}>'.format(self.title)
 
+    def to_json(self, list_view = False):
+
+        """
+        Return a dictionary of information of this movie
+        """
+        return {
+            'id' : self.id,
+            'title' : self.title,
+            'last_air' : self.last_air,
+            'running_time' : self.running_time,
+            'first_air' : self.first_air,
+            'creators' : self.creators,
+            'characters' : self.characters,
+            'image' : self.image
+        }
 
 class Creators(db.Model):
 
     """
-    DC creators with attirubtes name, occupation, birth date, birth place
+    DC creators with attirubtes title, occupation, birth date, birth place
     first publication made (if applicable), and other information
     """
 
     __tablename__ = 'creators'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50))
-    ocupation = db.Column(db.String(50))
-    birth_year = db.Column(db.String(50))
-    birth_place = db.Column(db.String(200))
+    id = db.Column(db.Integer)
+    title = db.Column(db.String(50), primary_key=True)
+    ocupations = db.Column(ARRAY(db.String(50)))
+    gender = db.Column(String(50))
+    birth_date = db.Column(db.String(50))
     first_publication = db.Column(db.String(100))
+    employers = db.Column(ARRAY(db.String(100)))
 
-    def __init__(self, name, occupation, birth_year, birth_place, first_publication):
+    def __init__(self, title, occupations, birth_date, first_publication, employers):        
         """
         Initialize a creator
         """
-        self.name = name
-        self.occupation = occupation
-        self.birth_year = birth_year
-        self.birth_place = birth_place
+        self.title = title
+        self.occupations = occupations
+        self.birth_date = birth_date
         self.first_publication = first_publication
+        slef.employers = db.Column(ARRAY(db.String(100)))
 
     def __repr__(self):
         """
         Return represenation of this creator in format
-        <name {}> where {} is the creator's name
+        <title {}> where {} is the creator's title
         """
-        return '<name {}>'.format(self.name)
+        return '<title {}>'.format(self.title)
+
+    def to_json(self, list_view = False):
+        """
+        Return a dictionary of information of this movie
+        """
+        return {
+            'id' : self.id,
+            'title' : self.title,
+            'birth_date' : self.birth_date,
+            'gender' : self.gender,
+            'first_publication' : self.first_publication,
+            'employers' : self.employers
+        }
