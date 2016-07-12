a76403187e0d8fe3b482fde59435d652cd1b44be
kiankris <kcv@utexas.edu>
2016-07-12 16:48:11 -0500

    Added characters controller

---
+++ models/js/controller/chractersCtrl.js
@@ -0,0 +1,54 @@
+app.controller('charactersCtrl',['$scope', '$http', function($scope, $http) {
+
+
+  $scope.charactersOptions = {
+    paginationPageSizes: [25, 50, 75, 100],
+    paginationPageSize: 25,
+    minRowsToShow: 4,
+    columnDefs:
+            [
+            {
+                field:'image', 
+                displayName:'Image',
+                cellTemplate: '<div class="ui-grid-cell-contents"><img src="{{ COL_FIELD }}" /></div>'
+            },
+            {
+                field:'name', 
+                displayName:'Name'
+            },
+            {
+                field:'alias',
+                displayName:'Alias'
+            },
+            {
+                field:'alignment',
+                displayName:'Alignment'
+            },
+            {
+                field:'affiliation',
+                displayName:'Affiliation'
+            },
+            {
+                field:'creators',
+                displayName:'Creators'
+            }
+        ]
+  };
+
+
+   // get real data via url
+
+  var getPage = function() {
+    // var url = host + "/data/characters";
+    var url = 'static/data/character.json';
+    $http.get(url, {cache: true})
+    .success(function (data) {
+      $scope.charactersOptions.data = data;
+    });
+  };
+
+  getPage();
+}
+]);
+
+
